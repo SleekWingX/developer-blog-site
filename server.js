ff9688c44f7af362298f3a663bd8d08ed202222d
SleekWingX <alexisyazzie365@gmail.com>
2024-06-10 16:03:43 -0600

    added routes data to the server.js file, and added empty css and javascript files for the client

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const { sequelize } = require('./config/config');
-const User = require('./models/User');
+const User = require('./models/User'); // Ensure User model is correctly used
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +13,9 @@ app.engine('hbs', require('express-handlebars')({
     extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
 // Set up session with Sequelize store
 app.use(session({
@@ -30,6 +33,22 @@ app.use(session({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Import routes
+const homeRoutes = require('./controllers/homeRoutes');
+const userRoutes = require('./controllers/api/userRoutes');
+const postRoutes = require('./controllers/api/postRoutes');
+
+// Use routes
+app.use('/', homeRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+
+// Basic error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
